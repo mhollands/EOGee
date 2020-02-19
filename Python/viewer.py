29d22211df10c97eb6e15b6d23a3d6f04b4ac25f
Matt Hollands <mattyh999@hotmail.com>
2020-02-18 21:48:00 -0800

    (i mean 700hz). Python viewer now converts 8 bit data into 16 bit data and displays it. Limit to 1000 samples on screen

--- Python/viewer.py
+++ Python/viewer.py
@@ -15,8 +15,13 @@ def init():
     return ln,
 
 def update(frame):
-	point = s.read(s.in_waiting)
-	[ydata.append(x) for x in point]
+	global ydata
+	num_bytes_available = int(np.floor(s.in_waiting/2.0)*2)
+	points_8bit = s.read(num_bytes_available)
+	points_16bit = [points_8bit[i] * 256 + points_8bit[i+1] for i in range(0,len(points_8bit), 2)]
+	[ydata.append(x) for x in points_16bit]
+	if(len(ydata) > 1000):
+		ydata = ydata[-1000:]
 	ax.set_ylim(min(ydata), max(ydata))
 	ax.set_xlim(0, len(ydata))
 	ln.set_data(range(0,len(ydata)), ydata)
