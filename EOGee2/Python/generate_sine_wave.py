a230205b83eb853c56d69cb5dbf4d234fc794fc6
Matt Hollands <mattyh999@hotmail.com>
2020-07-05 21:57:39 -0700

    fix typo

--- EOGee2/Python/generate_sine_wave.py
+++ EOGee2/Python/generate_sine_wave.py
@@ -9,4 +9,9 @@ fsample = ns * fsine
 
 i = (np.sin(2*np.pi*np.arange(ns)/ns) * (2**(nbits-1)-1)).astype(int) + 2**(nbits-1)
 q = (np.cos(2*np.pi*np.arange(ns)/ns) * (2**(nbits-1)-1)).astype(int) + 2**(nbits-1)
-print(np.array2string(d, separator=',').replace("\n", ""))
+
+print("in phase")
+print(np.array2string(i, separator=',').replace("\n", ""))
+
+print("quad phase")
+print(np.array2string(q, separator=',').replace("\n", ""))
